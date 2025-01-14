7a2a043aad7d19990cfd6421f2c2ff7512c0b81c
harispriantury <prianturyharis@gmail.com>
2025-01-14 20:16:16 +0700

    [TEST] Test Routing

--- src/pages/raincoat/Raincoat.tsx
+++ src/pages/raincoat/Raincoat.tsx
@@ -1,3 +1,21 @@
+import { Button } from "primereact/button";
+
+import { useNavigate } from "react-router-dom";
+
 export const Raincoat = () => {
-  return <div>Landing Page untuk jas hujan</div>;
+  const nav = useNavigate();
+  const handleNavigate = () => {
+    nav("/crystal-roman-bracelet");
+  };
+  return (
+    <div>
+      <Button
+        label="Navigate"
+        onClick={() => {
+          handleNavigate();
+        }}
+      />
+      <div>Landing Page untuk jas hujan</div>;
+    </div>
+  );
 };
